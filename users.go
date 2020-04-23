66d971a0a7b174c3815f67ce542f9b6811610b2d
gocmto <gocmto@gmail.com>
2020-04-23 08:22:19 +0300

    added "users"

---
+++ users.go
@@ -0,0 +1,116 @@
+package main
+
+// https://tutorialedge.net/golang/parsing-json-with-golang/
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+/**
+{
+  "users": [
+    {
+      "name": "Elliot",
+      "type": "Reader",
+      "age": 23,
+      "social": {
+        "facebook": "https://facebook-one.com",
+        "twitter": "https://twitter-one.com"
+      }
+    },
+    {
+      "name": "Fraser",
+      "type": "Author",
+      "age": 17,
+      "social": {
+        "facebook": "https://facebook-two.com",
+        "twitter": "https://twitter-two.com"
+      }
+    }
+  ]
+}
+*/
+
+// Users struct which contains
+// an array of users
+type Users struct {
+	Users []User `json:"users"`
+}
+
+// User struct which contains a name
+// a type and a list of social links
+type User struct {
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int    `json:"Age"`
+	Social Social `json:"social"`
+}
+
+// Social struct which contains a
+// list of links
+type Social struct {
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
+}
+
+func main() {
+	// Open our jsonFile
+	jsonFile, err := os.Open("users.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our Users array
+	var users Users
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &users)
+
+	// we iterate through every user within our users array and
+	// print out the user Type, their name, and their facebook url
+	// as just an example
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Type: " + users.Users[i].Type)
+		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+	}
+}
+
+/**
+{
+  "users": [
+    {
+      "name": "Elliot",
+      "type": "Reader",
+      "age": 23,
+      "social": {
+        "facebook": "https://facebook-one.com",
+        "twitter": "https://twitter-one.com"
+      }
+    },
+    {
+      "name": "Fraser",
+      "type": "Author",
+      "age": 17,
+      "social": {
+        "facebook": "https://facebook-two.com",
+        "twitter": "https://twitter-two.com"
+      }
+    }
+  ]
+}
+*/
